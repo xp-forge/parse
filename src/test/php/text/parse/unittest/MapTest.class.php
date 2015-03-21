40614fbc22c46db028313652283a27fc63e10b0d
Timm Friebe <thekid@thekid.de>
2015-03-21 12:13:43 +0100

    Add test for README.md-example

---
+++ src/test/php/text/parse/unittest/MapTest.class.php
@@ -0,0 +1,49 @@
+<?php namespace text\parse\unittest;
+
+use text\parse\Rules;
+use text\parse\rules\Repeated;
+use text\parse\rules\Sequence;
+use text\parse\rules\Token;
+use text\parse\rules\Apply;
+use text\parse\rules\AnyOf;
+use text\parse\rules\Collect;
+
+/**
+ * Verifies example on front page works
+ *
+ * @see   https://github.com/xp-forge/parse/blob/master/README.md
+ */
+class MapTest extends \unittest\TestCase {
+  private $syntax;
+
+  /**
+   * Declares syntax
+   */
+  public function setUp() {
+    $this->syntax= newinstance('text.parse.Syntax', [], [
+      'rules' => function() { return new Rules([
+        new Repeated(
+          new Sequence([new Token(T_STRING), new Token(':'), new Apply('val')], function($values) {
+            return [$values[0] => $values[2]];
+          }),
+          new Token(','),
+          Collect::$IN_MAP
+        ),
+        'val' => new AnyOf([
+          T_CONSTANT_ENCAPSED_STRING => function($values) { return substr($values[0], 1, -1); },
+          T_STRING                   => function($values) { return constant($values[0]); },
+          T_DNUMBER                  => function($values) { return (double)$values[0]; },
+          T_LNUMBER                  => function($values) { return (int)$values[0]; }
+        ])
+      ]); }
+    ]);
+  }
+
+  #[@test]
+  public function readme_example() {
+    $this->assertEquals(
+      ['a' => 1, 'b' => 2.0, 'c' => true, 'd' => 'D'],
+      $this->syntax->parse(new StringInput('a: 1, b: 2.0, c: true, d: "D"'))
+    );
+  }
+}
