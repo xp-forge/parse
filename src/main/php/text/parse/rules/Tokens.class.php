75bede8140da25d8da9b925865810815e2fa399f
Timm Friebe <thekid@thekid.de>
2015-03-21 14:55:05 +0100

    Use isset() instead of in_array()

--- src/main/php/text/parse/rules/Tokens.class.php
+++ src/main/php/text/parse/rules/Tokens.class.php
@@ -19,7 +19,7 @@ class Tokens extends \text\parse\Rule {
    * @param  var... $tokens
    */
   public function __construct() {
-    $this->tokens= func_get_args();
+    $this->tokens= array_flip(func_get_args());
   }
 
   /**
@@ -31,7 +31,7 @@ class Tokens extends \text\parse\Rule {
    * @return text.parse.Consumed
    */
   public function consume($rules, $tokens, $values) {
-    while (in_array($tokens->token()[0], $this->tokens)) {
+    while (isset($this->tokens[$tokens->token()[0]])) {
       $token= $tokens->token();
       $values[]= is_array($token) ? $token[1] : $token;
       $tokens->forward();
