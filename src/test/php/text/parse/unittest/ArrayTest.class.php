c1a5316a396be667219985406b1b04cb9b6a9309
Timm Friebe <thekid@thekid.de>
2015-03-22 17:33:26 +0100

    Add integration tests for parsing arrays

---
+++ src/test/php/text/parse/unittest/ArrayTest.class.php
@@ -0,0 +1,81 @@
+<?php namespace text\parse\unittest;
+
+use text\parse\Rules;
+use text\parse\Tokenized;
+use text\parse\rules\Sequence;
+use text\parse\rules\Apply;
+use text\parse\rules\Token;
+use text\parse\rules\Repeated;
+use text\parse\rules\Match;
+use text\parse\rules\OneOf;
+
+class ArrayTest extends \unittest\TestCase {
+  private $syntax;
+
+  /**
+   * Declares syntax
+   */
+  public function setUp() {
+    $this->syntax= newinstance('text.parse.Syntax', [], [
+      'rules' => function() { return new Rules([
+        new Sequence([new Apply('expr')], function($values) { return $values[0]; }),
+        'expr' => new OneOf([
+          new Match([
+            T_LNUMBER => function($values) { return (int)$values[0]; },
+            '[' => new Sequence(
+              [new Repeated(new Apply('expr'), new Token(',')), new Token(']')],
+              function($values) { return $values[1]; }
+            )
+          ]),
+          new Sequence(
+            [new Token(T_STRING), new Token(T_DOUBLE_COLON), new Token(T_CLASS)],
+            function($values) { return \xp::nameOf($values[0]); }
+          ),
+          new Token(T_STRING)
+        ])
+      ]); }
+    ]);
+  }
+
+  #[@test]
+  public function empty_array() {
+    $tokens= new Tokenized('[]');
+    $this->assertEquals([], $this->syntax->parse($tokens));
+  }
+
+  #[@test]
+  public function array_of_ints() {
+    $tokens= new Tokenized('[1, 2, 3]');
+    $this->assertEquals([1, 2, 3], $this->syntax->parse($tokens));
+  }
+
+  #[@test]
+  public function array_of_strings() {
+    $tokens= new Tokenized('[hello, world]');
+    $this->assertEquals(['hello', 'world'], $this->syntax->parse($tokens));
+  }
+
+  #[@test]
+  public function array_of_classes() {
+    $tokens= new Tokenized('[Object::class, Throwable::class]');
+    $this->assertEquals(['lang.Object', 'lang.Throwable'], $this->syntax->parse($tokens));
+  }
+
+  #[@test]
+  public function nested_array() {
+    $tokens= new Tokenized('[[hello]]');
+    $this->assertEquals([['hello']], $this->syntax->parse($tokens));
+  }
+
+  #[@test]
+  public function nested_empty_array() {
+    $tokens= new Tokenized('[hello, []]');
+    $this->assertEquals(['hello', []], $this->syntax->parse($tokens));
+  }
+
+  #[@test]
+  public function nested_arrays() {
+    $tokens= new Tokenized('[[hello, world], [1, 2]]');
+    $this->assertEquals([['hello', 'world'], [1, 2]], $this->syntax->parse($tokens));
+  }
+}
